7e3388e15300b26eb371ee126c6cc99c5d9066a4
VictorIvanow777 <147000034+VictorIvanow777@users.noreply.github.com>
2025-06-20 18:02:43 +0300

    Delete db directory

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    completed BOOLEAN DEFAULT FALSE
-);
-
-INSERT INTO tasks (title, completed) VALUES
-('Първа задача', FALSE),
-('Втора задача', TRUE);
