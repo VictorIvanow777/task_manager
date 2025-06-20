963856b3054dc06ef570eee7464b91669e9ebc85
VictorIvanow777 <147000034+VictorIvanow777@users.noreply.github.com>
2025-06-20 17:43:17 +0300

    Add files via upload

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO tasks (title, completed) VALUES
+('Първа задача', FALSE),
+('Втора задача', TRUE);
